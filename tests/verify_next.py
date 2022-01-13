7d4a581596fa1a1e6cffe7b8b281f1a014505743
Arthus Leroy <aleroy@hyperstone.com>
2022-01-13 10:58:54 +0100

    [TEST] Add test verifying _next id fetching still works

---
+++ tests/verify_next.py
@@ -0,0 +1,31 @@
+# Verify all the requests using the _next framework work
+
+from os import environ as env
+from sys import path
+from os.path import dirname, abspath, join
+from asyncio import run
+
+path.append(join(dirname(abspath(__file__)), ".."))
+
+from holoai_api import HoloAI_API
+from aiohttp import ClientSession
+
+from typing import Union
+
+if "HAI_USERNAME" not in env or "HAI_PASSWORD" not in env:
+	raise RuntimeError("Please ensure that HAI_USERNAME and HAI_PASSWORD are set in your environment")
+
+username = env["HAI_USERNAME"]
+password = env["HAI_PASSWORD"]
+
+async def main():
+	async with ClientSession() as session:
+		api = HoloAI_API(session)
+
+		account_key = await api.high_level.login(username, password)
+
+		print(await api.low_level.get_home())
+#		print(await api.low_level.get_story(story_id))
+		print(await api.low_level.get_tunes())
+
+run(main())
